42f60723a91c8a4deed3370722c43398c77b6941
mkMohit1 <49286417+mkMohit1@users.noreply.github.com>
2024-05-07 15:49:02 +0530

    Time: 47 ms (85.86%), Space: 49.2 MB (22.60%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let count;
+    //nums.sort((a,b)=>a-b);
+    for(let j=0;j<nums.length;j++){
+        count =0;
+    for(let i=0;i<nums.length;i++ ){
+        let temp =null;
+        if(nums[i] == val){
+            if(i<nums.length-1){
+                temp = nums[i+1];
+                nums[i+1] = nums[i];
+                nums[i] = temp;
+            }
+        }else{
+            count++;
+        }
+    }
+    }
+return count;
+};
